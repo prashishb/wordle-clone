1f42b88cae18559d9e3eabeafc1849e123fa00c5
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2022-02-18 10:31:40 -0500

    board data state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { wordList } from './constants/data';
 
 function App() {
+  const [boardData, setBoardData] = useState(
+    JSON.parse(localStorage.getItem('board-data'))
+  );
+  const [charArr, setCharArr] = useState([]);
+
+  useEffect(() => {
+    if (!boardData || !boardData.solution) {
+      let alphabetIndex = Math.floor(Math.random() * 26);
+      let wordIndex = Math.floor(
+        Math.random() * wordList[String.fromCharCode(97 + alphabetIndex)].length
+      );
+      let newBoardData = {
+        ...boardData,
+        solution: wordList[String.fromCharCode(97 + alphabetIndex)][wordIndex],
+        rowIndex: 0,
+        boardWords: [],
+        boardRowStatus: [],
+        presentCharArray: [],
+        absentCharArray: [],
+        correctCharArray: [],
+        status: 'IN_PROGRESS',
+      };
+    }
+  }, []);
+
   return <div>Hello</div>;
 }
 
